5deb27f82d673f49cb87d6aa3afa6e4b67aa8c94
Angel <angel92.addcs@gmail.com>
2021-09-29 17:20:17 -0500

    big changes

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function getForumData() {
                 res.json().then(data => {
                     last_fetch = data;
                     resolve(data)
-                    forumBasicInfo()
                 })
             })
         } else {
@@ -21,7 +20,6 @@ function getForumData() {
 function hamming_distance(string1, string2) {
 	var dist_counter = 0
     for (let n = 0; n < string1.length; n++) {
-        
         if (string1[n] != string2[n]) dist_counter += 1
     }
 	return dist_counter
@@ -34,6 +32,8 @@ function forumBasicInfo() {
             const topic = data[i];
             total_posts += topic.posts.length;
         }
+
+        console.log(data)
 
         document.getElementById('ForumInfo').innerHTML = `${data.length} topics; ${total_posts} posts`;
     })
@@ -50,23 +50,26 @@ var search = {
             input.value = ""
         },2000)
     },
-    topic: function() {
+    topic: function(suggestion) {
         output.innerHTML = "searching topic.."
         
         getForumData().then(data => {
+            
             var returntopic;
             var def = 5;
-            for (let i = 0; i < data.length; i++) {
-                const topic = data[i];
 
-                if (hamming_distance(topic.name,input.value) <= def) {
-                    def--;
-                    returntopic = topic.name
-                }
-
+            for (let i = 0; i < 2; i++) {
+                data.forEach(topic => {
+                    if (hamming_distance(topic.name,input.value) <= def) {
+                        def--;
+                        returntopic = topic.name
+                    }
+                });
             }
+
             return returntopic;
         }).then(topicname => {
+            document.getElementById('ForumOuput').innerHTML += `<p>${topicname}</p>`
             output.innerHTML = `Did you mean ${topicname}?`
             input.value = "";
         })
@@ -76,18 +79,27 @@ var search = {
         getForumData().then(data => {
             var returntopic;
             var def = 5;
-            for (let i = 0; i < data.length; i++) {
-                const topic = data[i];
-
-                if (hamming_distance(topic.name,input.value) <= def) {
-                    def--;
-                    returntopic = topic.name
-                }
-
+            
+            for (let i = 0; i < 2; i++) {
+                data.forEach(topic => {
+                    if (hamming_distance(topic.name,input.value) <= def) {
+                        def--;
+                        returntopic = topic.name
+                    }
+                });
             }
-            return returntopic;
-        }).then(topicname => {
-            if (topicname) suggestion.innerHTML = `Did you mean ${topicname}?`
+
+            if (!returntopic) return undefined
+
+            return {
+                "name":returntopic,
+                "hdist":hamming_distance(returntopic,input.value),
+                "def":def
+            };
+            
+        }).then(topic => {
+            if (topic && topic.name) suggestion.innerHTML = `You meant <a onclick="document.getElementById('MInput').value = '${topic.name}'">${topic.name}</a>? hm: ${topic.hdist} || def: ${topic.def}`
+            else suggestion.innerHTML = ""
         })
     }
 }
