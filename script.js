e9489e9a23077ce1f3b2cb28be67c62e0bdf763a
Angel <angel92.addcs@gmail.com>
2022-08-08 23:20:21 -0500

    c

--- script.js
+++ script.js
@@ -1,90 +1,108 @@
 const input = document.getElementById('MInput')
-const output = document.getElementById('MOutput')
+const output = {
+    element: document.getElementById('OutputList'),
+    contents:[],
+    print: function(text, offset) {
+        text = `<p>${text}</p>`;
+        var result = this.element.innerHTML + text;
+        if (offset == 1) result = text + this.element.innerHTML;
 
-function getData(api_point) {
-    return new Promise(function(resolve, reject) {
-        fetch("https://angeldc943.repl.co/api/"+api_point).then(res => {
-            res.json().then(data => {
-                resolve(data)
-            })
-        })
-    })
-}
-
-function hamming_distance(string1, string2) {
-	var dist_counter = 0
-    for (let n = 0; n < string1.length; n++) {
-        if (string1[n] != string2[n]) 
-            dist_counter += 1
-    }
-    //console.log(string2.substring(string1.length))
-
-	return dist_counter
-}
-
-function BasicInfo() {
-    getData("forum/get").then(data => {
-        var total_posts = 0;
-        for (let i = 0; i < data.length; i++) {
-            const topic = data[i];
-            total_posts += topic.posts.length;
-        }
-
-        console.log(data)
-
-        document.getElementById('ForumInfo').innerHTML = `${data.length} topics; ${total_posts} posts`;
-    })
-}
-
-BasicInfo()
-
-var search = {
-    find: function(suggestion) {
-        output.innerHTML = `Searching ${input.value}..`
+        this.element.innerHTML = result;
+        this.element.scrollTop = this.element.scrollHeight;
         
-        getData("users/get").then(data => {
-            document.getElementById('ForumOuput').innerHTML += `<p>${input.value}</p>`
-            output.innerHTML = "";
-            input.value = "";
-        })
+        if (offset == 1) {
+            this.contents.unshift(text);
+        } else {
+            this.contents.push(text);
+        }
     },
-    suggestion: function() {
-        var suggestion = document.getElementById('MSuggest')
-        getData("users/get").then(userdata => {
-            var data = [];
-            userdata.forEach(user => {
-                data.push(user.name);
-            })
+    clear: function(amount = 0) {
+        if (amount > 0) {
+            var removednumber = this.element.innerHTML.length;
             
-            var returntopic;
-            var def = input.value.length*10;
-            var user_element = data.findIndex( (element) => element.toLowerCase() == input.value.toLowerCase());
-
-            if (user_element != -1) returntopic = data[user_element]
-
-            if (!returntopic) for (let d = 0; d < data.length; d++) {
-                const user = data[d];
-                const dist = hamming_distance(user.toLowerCase(),input.value.toLowerCase());
-                //console.log(def)
-                if (dist < user.length && dist <= def) {
-                    def--;
-                    returntopic = user
-                }
+            for (let i = this.contents.length; i > this.contents.length-amount; i--) {
+                if (this.contents[i-1]) removednumber -= this.contents[i-1].length;
             }
+            this.contents.splice(this.contents.length-amount, this.contents.length)
 
-            if (!returntopic) return undefined
+            this.element.innerHTML = this.element.innerHTML.substring(0, removednumber);
+        }
+        else {
+            this.element.innerHTML = ""
+        }
+    },
+    set: function(list, disableScroll) {
+        if (typeof(list) != "object") return;
 
-            return {
-                "name":returntopic,
-                "hdist":hamming_distance(returntopic,input.value),
-                "def":def
-            };
-
-        }).then(topic => {
-            var s = ""
-            if (topic && topic.name) s = `You meant <a onclick="document.getElementById('MInput').value = '${topic.name}'">${topic.name}</a>? hm: ${topic.hdist} || def: ${topic.def}`
-            
-            suggestion.innerHTML = s
-        })
+        this.element.innerHTML = "<p>"+list.join("</p><p>")+"</p>";
+        if (!disableScroll) this.element.scrollTop = this.element.scrollHeight;
+        this.contents = list;
     }
 }
+
+function char_distance(string, targetstring) {
+    var dist_counter = 0
+    string = String(string).toLowerCase();
+    targetstring = String(targetstring).toLowerCase();
+    
+    for (let n = 0; n < targetstring.length; n++) {
+        if (targetstring[n] != string[n] && string[n] != undefined) 
+        dist_counter += 1
+    }
+    return dist_counter
+}
+
+var data = [ // Test data
+    "Andec",
+    "Computer",
+    "Forum",
+    "Blog",
+    "Website",
+    "Server",
+    "Data",
+    "Stuff",
+    "Internet",
+    "Cones",
+    "Post",
+    "Archive",
+    "Date",
+    "Sector A",
+    "Sector B",
+    "Test Chamber A5"
+]
+
+fetch("https://angeldc943.repl.co/api/users/get?value=name").then(res => {
+    return res.json();
+}).then(users => {
+    data = [...data, ...users]
+    suggestion();
+})
+
+function suggestion() {
+    var suggestionlist = [];
+    var distlist = []
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        var dist = char_distance(input.value, element);
+        suggestionlist.push(element);
+        distlist.push(element + "; dist:" + dist);
+    }
+
+    distlist.sort((element_a, element_b) => {
+        const a = element_a.split("; dist:")[1];
+        const b = element_b.split("; dist:")[1];
+        return a-b;
+    })
+    
+    for (let index = 0; index < distlist.length; index++) {
+        distlist[index] = distlist[index].split("; dist:")[0];
+    }
+
+    output.set(distlist, true);
+}
+
+/*fetch('./data.json').then(res => res.json()).then(data => {
+    suggestion = function() {
+        console.log(data)
+    }
+});*/
